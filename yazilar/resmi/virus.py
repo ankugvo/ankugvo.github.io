c15ad8910d87bab14833bbd6647f1488ce755915
Can Koyuncu <cankoyuncu005@duck.com>
2026-04-19 10:50:21 +0300

    ;)

---
+++ yazilar/resmi/virus.py
@@ -0,0 +1,13 @@
+from webbrowser import open
+from time import sleep
+x = 0
+open(r"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.giantfreakinrobot.com%2Fwp-content%2Fuploads%2F2020%2F03%2Fvirus-scaled.jpg&f=1&nofb=1&ipt=e3841dfc4fd2d32bce4c5772906a5a5a8a5818a5be1cd9556599896456ee1119")
+input("WARNING: This is a virus. Press enter to open at your own risk. ")
+while True:
+    if x < 0.0001:
+        sleep(x)
+        open(r"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.giantfreakinrobot.com%2Fwp-content%2Fuploads%2F2020%2F03%2Fvirus-scaled.jpg&f=1&nofb=1&ipt=e3841dfc4fd2d32bce4c5772906a5a5a8a5818a5be1cd9556599896456ee1119")
+    else:
+        sleep(x)
+        open(r"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.giantfreakinrobot.com%2Fwp-content%2Fuploads%2F2020%2F03%2Fvirus-scaled.jpg&f=1&nofb=1&ipt=e3841dfc4fd2d32bce4c5772906a5a5a8a5818a5be1cd9556599896456ee1119")
+        x /= 1.5
