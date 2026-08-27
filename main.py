ce690ba7cf3906a246fcdd5fbc5e5d301242342f
Can Koyuncu <icy-flip-subdivide@duck.com>
2026-08-27 16:22:35 +0300

    ds

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-p = "fhl"
-x = []
-while p != "q":
-    p = input()
-    x.append(p)
-while "" in x:
-    del x[x.index("")]
-while "q" in x:
-    del x[x.index("q")]
-x.sort()
-for i in x:
-    print(f"                <tr><td>{i}</td></tr>")
+print("č".upper())
